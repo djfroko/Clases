bc04e7d1e3b8e92a893056f64298cf1eb77179ad
djfroko <djfroko@gmail.com>
2018-06-13 11:34:10 +0200

    copiado lo de gustavo

--- src/com/cice/clases/coches/Coche.java
+++ src/com/cice/clases/coches/Coche.java
@@ -2,35 +2,41 @@ package com.cice.clases.coches;
 
 public class Coche {
 
-    //atributos de la clase normalmente privados
+    //atributos de la clase normalmente privados...
     private String color;
     private int potencia;
     private String modelo;
     private String bastidor;
     private String marca;
 
-    //metodos
-    public void arrancar() {
-        System.out.println("estoy arrancando....");
+
+    public void arrancar(){
+        System.out.println("Estoy arrancando...");
+        System.out.println("Motor arrancado...");
     }
 
-    public void acelerar() {
-        System.out.println("estoy acelerandoooooo..");
+    public void acelerar(){
+        String cadena = "Acelerand";
+        int numeros = potencia/10;
+        for(int i = 0; i <= numeros; i++){
+            cadena+="o";
+        }
+        System.out.println(cadena+"...");
     }
 
-    public void frenar() {
-        System.out.println("estoy frenando...");
+    public void frenar(){
+        System.out.println("Frenando...");
     }
 
-    public void parar() {
-        System.out.println("apagando motor...");
-        System.out.println("motor parado..");
+    public void parar(){
+        System.out.println("Apagando motor...");
+        System.out.println("Motor parado...");
     }
 
     public void setPotencia(int potenciaDada) {
-        if (potenciaDada > 0) {
+        if(potenciaDada > 0) {
             potencia = potenciaDada;
         }
-
     }
+
 }
